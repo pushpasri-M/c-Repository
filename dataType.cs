b7e416751f22624fd49f2d9c76a31f43871a93be
Pushpasri M <127647687+pushpasri-M@users.noreply.github.com>
2024-07-07 12:48:57 -0400

    Create dataType.cs
    this include the DataTypes in C#

---
+++ dataType.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearningHowToProgram : MonoBehaviour
+{
+    float speed = 23.90f;
+    int number = 456;
+    string name = Pushpasri;
+    bool isDead = true;
+    double riger = 237983743.743387427409274;
+
+}
