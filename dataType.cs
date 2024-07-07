eece9186ca203063eb4a18beab6446255039f6b4
Pushpasri M <127647687+pushpasri-M@users.noreply.github.com>
2024-07-07 12:58:35 -0400

    Update dataType.cs
    update

--- dataType.cs
+++ dataType.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class LearningHowToProgram : MonoBehaviour
 {
     float speed = 23.90f;
+    //float this usually used decimal number
     int number = 456;
+    //integer is a whole number in the program
     string name = Pushpasri;
+    // strings are the sequence of characters 
     bool isDead = true;
+    // Boolean refers to true or false;
     double riger = 237983743.743387427409274;
+    //double is usually enhanced version of float with precess
+    char onechar = 'a';
+    // character 
 
 }
